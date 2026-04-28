3580de966b8e17b3df2a32436bbc3cdb1a5a6488
sarab-90 <sarabakash.12345@gmail.com>
2026-04-28 13:11:35 +0300

    add helmet middleware for enhanced security headers - Implemented auth routes for user registration, login, and logout with validation and rate limiting. - Added token generation and verification utilities for access and refresh tokens using JWT.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,35 @@
 import connectDB from "./src/config/db.js";
 import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./src/routes/authRoutes.js";
+
+import {errorHandler} from "./src/middleware/errorHandlerMiddleware.js";
+import {globalRateLimitMiddleware} from "./src/middleware/globalRateLimitMiddleware.js";
+
 dotenv.config();
 connectDB();
-console.log('Starting server...   hi sarab');
+
+const app = express();
+app.use(express.json());
+app.use(globalRateLimitMiddleware);
+app.use(helmet());
+app.use(cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+// Routes
+app.use('/api', authRoutes);
+
+
+app.use(errorHandler);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
---
+++ server/src/utils/cookiesUtils.js
@@ -0,0 +1,21 @@
+import cookie from 'cookie-parser';
+export const setAccessTokenCookie = (res, token) => {
+    res.cookie('accessToken', token, {
+        httpOnly: true,
+        secure: true,
+        satisfies: 'Strict',
+        maxAge: 24 * 60 * 60 * 1000, 
+    });
+}
+export const setRefreshTokenCookie = (res, token) => {
+    res.cookie('refreshToken', token, {
+        httpOnly: true,
+        secure: true,
+        satisfies: 'Strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+}
+export const clearAuthCookies = (res) => {
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+}
